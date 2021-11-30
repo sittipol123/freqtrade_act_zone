d2dbaf7b97c9428c568022814dfa6995aa00daa1
sittipol.p <sittipol123@gmail.com>
2021-11-30 09:56:42 +0700

    Added pinescripts and other Pyton scripts

---
+++ hyperopts/bbrsi_opt.py
@@ -0,0 +1,209 @@
+# pragma pylint: disable=missing-docstring, invalid-name, pointless-string-statement
+
+import talib.abstract as ta
+from pandas import DataFrame
+from typing import Dict, Any, Callable, List
+from functools import reduce
+
+from skopt.space import Categorical, Dimension, Integer, Real
+
+import freqtrade.vendor.qtpylib.indicators as qtpylib
+from freqtrade.optimize.hyperopt_interface import IHyperOpt
+
+class_name = 'BBRSIOPT'
+
+
+class BBRSIOPT(IHyperOpt):
+    """
+    Default hyperopt provided by freqtrade bot.
+    You can override it with your own hyperopt
+    """
+
+    @staticmethod
+    def populate_indicators(dataframe: DataFrame, metadata: dict) -> DataFrame:
+
+        dataframe['rsi'] = ta.RSI(dataframe)
+        dataframe['sell-rsi'] = ta.RSI(dataframe)
+
+        # Bollinger bands
+        bollinger1 = qtpylib.bollinger_bands(qtpylib.typical_price(dataframe), window=20, stds=1)
+        dataframe['bb_lowerband1'] = bollinger1['lower']
+        dataframe['bb_middleband1'] = bollinger1['mid']
+        dataframe['bb_upperband1'] = bollinger1['upper']
+
+        bollinger2 = qtpylib.bollinger_bands(qtpylib.typical_price(dataframe), window=20, stds=2)
+        dataframe['bb_lowerband2'] = bollinger2['lower']
+        dataframe['bb_middleband2'] = bollinger2['mid']
+        dataframe['bb_upperband2'] = bollinger2['upper']
+
+        bollinger3 = qtpylib.bollinger_bands(qtpylib.typical_price(dataframe), window=20, stds=3)
+        dataframe['bb_lowerband3'] = bollinger3['lower']
+        dataframe['bb_middleband3'] = bollinger3['mid']
+        dataframe['bb_upperband3'] = bollinger3['upper']
+
+        bollinger4 = qtpylib.bollinger_bands(qtpylib.typical_price(dataframe), window=20, stds=4)
+        dataframe['bb_lowerband4'] = bollinger4['lower']
+        dataframe['bb_middleband4'] = bollinger4['mid']
+        dataframe['bb_upperband4'] = bollinger4['upper']
+
+        return dataframe
+
+    @staticmethod
+    def buy_strategy_generator(params: Dict[str, Any]) -> Callable:
+        """
+        Define the buy strategy parameters to be used by hyperopt
+        """
+        def populate_buy_trend(dataframe: DataFrame, metadata: dict) -> DataFrame:
+            """
+            Buy strategy Hyperopt will build and use
+            """
+            conditions = []
+            # GUARDS AND TRENDS
+            if 'rsi-enabled' in params and params['rsi-enabled']:
+                conditions.append(dataframe['rsi'] > params['rsi-value'])
+
+            # TRIGGERS
+            if 'trigger' in params:
+                if params['trigger'] == 'bb_lower1':
+                    conditions.append(dataframe['close'] < dataframe['bb_lowerband1'])
+                if params['trigger'] == 'bb_lower2':
+                    conditions.append(dataframe['close'] < dataframe['bb_lowerband2'])
+                if params['trigger'] == 'bb_lower3':
+                    conditions.append(dataframe['close'] < dataframe['bb_lowerband3'])
+                if params['trigger'] == 'bb_lower4':
+                    conditions.append(dataframe['close'] < dataframe['bb_lowerband4'])
+
+            if not conditions:
+                pass
+            else:
+                dataframe.loc[
+                    reduce(lambda x, y: x & y, conditions),
+                    'buy'] = 1
+
+            return dataframe
+
+        return populate_buy_trend
+
+    @staticmethod
+    def indicator_space() -> List[Dimension]:
+        """
+        Define your Hyperopt space for searching strategy parameters
+        """
+        return [
+            Integer(5, 50, name='rsi-value'),
+            Categorical([True, False], name='rsi-enabled'),
+            Categorical(['bb_lower1', 'bb_lower2', 'bb_lower3', 'bb_lower4'], name='trigger')
+        ]
+
+    @staticmethod
+    def sell_strategy_generator(params: Dict[str, Any]) -> Callable:
+        """
+        Define the sell strategy parameters to be used by hyperopt
+        """
+        def populate_sell_trend(dataframe: DataFrame, metadata: dict) -> DataFrame:
+            """
+            Sell strategy Hyperopt will build and use
+            """
+            # print(params)
+            conditions = []
+            # GUARDS AND TRENDS
+            if 'sell-rsi-enabled' in params and params['sell-rsi-enabled']:
+                conditions.append(dataframe['sell-rsi'] > params['sell-rsi-value'])
+
+            # TRIGGERS
+            if 'sell-trigger' in params:
+                if params['sell-trigger'] == 'sell-bb_lower1':
+                    conditions.append(dataframe['close'] > dataframe['bb_lowerband1'])
+                if params['sell-trigger'] == 'sell-bb_middle1':
+                    conditions.append(dataframe['close'] > dataframe['bb_middleband1'])
+                if params['sell-trigger'] == 'sell-bb_upper1':
+                    conditions.append(dataframe['close'] > dataframe['bb_upperband1'])
+
+            if not conditions:
+                pass
+            else:
+                dataframe.loc[
+                    reduce(lambda x, y: x & y, conditions),
+                    'sell'] = 1
+
+            return dataframe
+
+        return populate_sell_trend
+
+    @staticmethod
+    def sell_indicator_space() -> List[Dimension]:
+        """
+        Define your Hyperopt space for searching sell strategy parameters
+        """
+        return [
+            Integer(30, 100, name='sell-rsi-value'),
+            Categorical([True, False], name='sell-rsi-enabled'),
+            Categorical(['sell-bb_lower1',
+                         'sell-bb_middle1',
+                         'sell-bb_upper1'], name='sell-trigger')
+        ]
+
+    @staticmethod
+    def generate_roi_table(params: Dict) -> Dict[int, float]:
+        """
+        Generate the ROI table that will be used by Hyperopt
+        """
+        roi_table = {}
+        roi_table[0] = params['roi_p1'] + params['roi_p2'] + params['roi_p3']
+        roi_table[params['roi_t3']] = params['roi_p1'] + params['roi_p2']
+        roi_table[params['roi_t3'] + params['roi_t2']] = params['roi_p1']
+        roi_table[params['roi_t3'] + params['roi_t2'] + params['roi_t1']] = 0
+
+        return roi_table
+
+    @staticmethod
+    def stoploss_space() -> List[Dimension]:
+        """
+        Stoploss Value to search
+        """
+        return [
+            Real(-0.5, -0.02, name='stoploss'),
+        ]
+
+    @staticmethod
+    def roi_space() -> List[Dimension]:
+        """
+        Values to search for each ROI steps
+        """
+        return [
+             Integer(10, 120, name='roi_t1'),
+             Integer(10, 60, name='roi_t2'),
+             Integer(10, 40, name='roi_t3'),
+             Real(0.01, 0.04, name='roi_p1'),
+             Real(0.01, 0.07, name='roi_p2'),
+             Real(0.01, 0.20, name='roi_p3'),
+        ]
+
+    def populate_buy_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        """
+        Based on TA indicators. Should be a copy of from strategy
+        must align to populate_indicators in this file
+        Only used when --spaces does not include buy
+        """
+        dataframe.loc[
+            (
+                (dataframe['rsi'] > 30) &
+                (dataframe["close"] < dataframe['bb_lowerband'] )
+            ),
+            'buy'] = 1
+
+        return dataframe
+
+    def populate_sell_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        """
+        Based on TA indicators. Should be a copy of from strategy
+        must align to populate_indicators in this file
+        Only used when --spaces does not include sell
+        """
+        dataframe.loc[
+            (
+
+                (dataframe["close"] > dataframe['bb_middleband'] )
+            ),
+            'sell'] = 1
+        return dataframe
